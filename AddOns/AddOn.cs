3cf35f2da9da507e12a45e0f7fe2f4dffcd6b3a7
Gustav Nilsson <gustavnilsson14@gmail.com>
2020-09-17 14:36:24 +0700

    added doc for AddOn.cs

--- AddOns/AddOn.cs
+++ AddOns/AddOn.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Base class for any specialized input logic, as well as modular Design, and View logic. An AddOn is the rough equivalent of an MVC's Controller, but highly specialized, and modular.
+/// </summary>
 public class AddOn : Design
 {
     public bool hasListeners = false;
